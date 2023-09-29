234c0ce3490b40459fa2f060ee85341094095db2
OssianN <ossian.northen@gmail.com>
2023-09-29 18:00:20 +0200

    fixes structure for nav component

---
+++ components/nav-bar/MultipleLinksItem.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import Link from 'next/link'
+import { CSSProperties, useState } from 'react'
+
+const MultipleLinksItem = () => {
+  const [hashtagListOpen, setHashtagListOpen] = useState<boolean>(false)
+
+  return (
+    <li
+      className="nav__item nav__item--multiple-container"
+      onMouseOver={() => setHashtagListOpen(true)}
+      onMouseOut={() => setHashtagListOpen(false)}
+    >
+      <p className="nav__item--multiple-title">
+        Hashtags <span className="nav__item--multiple-arrow">&#8250;</span>
+      </p>
+      <div
+        className="nav__item-list"
+        style={
+          {
+            visibility: hashtagListOpen
+              ? 'visible'
+              : 'var(--nav-item-list-hidden)',
+          } as CSSProperties
+        }
+      >
+        <Link className="nav__link" href="/hashtags/pressatLage">
+          #pressatläge
+        </Link>
+        <Link className="nav__link" href="/hashtags/viVagrarBrytaLagen">
+          #vivägrarbrytalagen
+        </Link>
+      </div>
+    </li>
+  )
+}
+
+export default MultipleLinksItem
