67a339d2fea9786bfa6ad7076f8d48dada8e12b3
OssianN <ossian.northen@gmail.com>
2023-10-02 19:27:10 +0200

    fixes styling

--- app/(standard-layout)/article/[slug]/page.tsx
+++ app/(standard-layout)/article/[slug]/page.tsx
@@ -24,38 +24,44 @@ const ArticlePage = async ({
 }: {
   params: { slug: string }
 }) => {
-  const { title, videoLink, contentImage, mainText, authorImage } = (
-    await client.withoutUnresolvableLinks.getEntries<BlogPostEntry>({
-      content_type: 'blogPost',
-      'fields.slug': slug,
-    })
-  ).items[0].fields
+  try {
+    const { title, videoLink, contentImage, mainText, authorImage } = (
+      await client.withoutUnresolvableLinks.getEntries<BlogPostEntry>({
+        content_type: 'blogPost',
+        'fields.slug': slug,
+      })
+    ).items[0].fields
 
-  return (
-    <div className="article__container">
-      <header className="main-header">
-        <BackToPressButton />
-        <h1 className="main-header__h1 article__heading">{title}</h1>
+    return (
+      <div className="article__container">
+        <header className="main-header">
+          <BackToPressButton />
+          <h1 className="main-header__h1 article__heading">{title}</h1>
 
-        <div className="main-header__hero-img">
-          <VideoIframe image={contentImage as ImageType} video={videoLink} />
+          <div className="main-header__hero-img">
+            <VideoIframe image={contentImage as ImageType} video={videoLink} />
+          </div>
+        </header>
+
+        <div className="grid-view article__body">
+          {documentToReactComponents(mainText)}
         </div>
-      </header>
-
-      <div className="grid-view article__body">
-        {documentToReactComponents(mainText)}
+        <div className="author__container">
+          <Image
+            className="author__img"
+            src={`https:${getImage(authorImage).fields?.file.url}`}
+            alt={getImage(authorImage).fields?.title ?? ''}
+            {...getImage(authorImage).fields?.file.details.image}
+          />
+          <h4 className="author__title">
+            {getImage(authorImage).fields?.title}
+          </h4>
+        </div>
       </div>
-      <div className="author__container">
-        <Image
-          className="author__img"
-          src={`https:${getImage(authorImage).fields?.file.url}`}
-          alt={getImage(authorImage).fields?.title ?? ''}
-          {...getImage(authorImage).fields?.file.details.image}
-        />
-        <h4 className="author__title">{getImage(authorImage).fields?.title}</h4>
-      </div>
-    </div>
-  )
+    )
+  } catch (e) {
+    return <></>
+  }
 }
 
 export default ArticlePage
