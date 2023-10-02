d23c67ba0a96a6e6e85f25581faf0083382a8ef0
OssianN <ossian.northen@gmail.com>
2023-10-02 13:40:30 +0200

    updates content for goal and demand

--- app/(standard-layout)/goal-demand/page.tsx
+++ app/(standard-layout)/goal-demand/page.tsx
@@ -1,19 +1,23 @@
 import Image from 'next/image'
 import goalImage from '@/images/goal.svg'
 import demandImage from '@/images/demand2.svg'
+import * as images from '@/images'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import ReactMarkdown from 'react-markdown'
-import { EntryFieldTypes } from 'contentful'
+import type { EntryFieldTypes } from 'contentful'
 import { client } from '@/contentful'
+import { getImage } from '@/utils'
 
 type GoalAndDemandEntrySkeleton = {
   contentTypeId: 'gaolAndDemandsPage'
   fields: {
     title: EntryFieldTypes.Text
+    image1: EntryFieldTypes.AssetLink
     paragraph: EntryFieldTypes.RichText
     link1: EntryFieldTypes.RichText
     bannerText: EntryFieldTypes.Text
     title2: EntryFieldTypes.Text
+    image2: EntryFieldTypes.AssetLink
     subTitle1: EntryFieldTypes.Text
     subTitle2: EntryFieldTypes.Text
     list1: EntryFieldTypes.RichText
@@ -25,10 +29,12 @@ type GoalAndDemandEntrySkeleton = {
 export const OmOss = async () => {
   const {
     title,
+    image1,
     paragraph,
     link1,
     bannerText,
     title2,
+    image2,
     subTitle1,
     subTitle2,
     list1,
@@ -43,7 +49,21 @@ export const OmOss = async () => {
       <header className=" main-header section1">
         <h1 className="main-header__h1">{title}</h1>
 
-        <Image className="main-header__hero-img" src={goalImage} alt="demand" />
+        <Image
+          className="goal-demand__img main-header__hero-img"
+          src={`https:${getImage(image1).fields?.file.url}`}
+          {...getImage(image1).fields?.file.details.image}
+          alt="demand"
+        />
+
+        <div className="goal-demand__banner">
+          <Image
+            className="goal-demand__banner-image"
+            src={images.goalDemandBanner}
+            alt="goal demand banner"
+          />
+          <h3 className="goal-demand__banner-text ">{bannerText}</h3>
+        </div>
 
         <div className="main-header__intro-paragraph main-p orange-text-parent">
           {documentToReactComponents(paragraph)}
@@ -54,15 +74,14 @@ export const OmOss = async () => {
         {documentToReactComponents(link1)}
       </div>
 
-      <h3 className="goal-demand__black-banner">{bannerText}</h3>
-
       <section className="main-header section2">
-        <h2 className="main-h2">{title2}</h2>
+        <h2 className="main-header__h1">{title2}</h2>
 
         <Image
           className="goal-demand__img main-header__hero-img"
-          src={demandImage}
-          alt="demand"
+          src={`https:${getImage(image2).fields?.file.url}`}
+          {...getImage(image2).fields?.file.details.image}
+          alt={getImage(image2).fields?.title ?? ''}
         />
 
         <div className="demand__list-wrapper grid-view">
--- images/index.js
+++ images/index.js
@@ -1,30 +1,20 @@
-import aboutUs from './aboutUs.svg'
 import at from './at.png'
 import banner from './banner.png'
-import demand2 from './demand2.svg'
 import facebook from './fb.png'
-import goalImage from './goal.svg'
-import history from './history.svg'
+import goalDemandBanner from './goalDemandBanner.svg'
 import insta from './insta.png'
 import linkedin from './linkedin.png'
-import press from './press.svg'
-import powerful from './powerful.svg'
 import thousand from './1000.svg'
 import xLogo from './xLogo.png'
 import youtube from './youtube.png'
 
 export {
-  aboutUs,
   at,
   banner,
-  demand2,
   facebook,
-  goalImage,
-  history,
+  goalDemandBanner,
   insta,
   linkedin,
-  press,
-  powerful,
   thousand,
   xLogo,
   youtube,
