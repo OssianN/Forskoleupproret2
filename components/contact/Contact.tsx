5c6dbafd06410c615b646d42d11b425494772445
OssianN <ossian.northen@gmail.com>
2023-10-02 13:58:40 +0200

    adds content to contact component

--- components/contact/Contact.tsx
+++ components/contact/Contact.tsx
@@ -1,88 +1,45 @@
-import './contact.scss'
 import Image from 'next/image'
-import * as img from '@/images'
+import { client } from '@/contentful'
+import { EntryFieldTypes } from 'contentful'
+import { getImage } from '@/utils'
+import './contact.scss'
 
-const Contact = () => {
+type ContactLinkEntries = {
+  contentTypeId: 'contactLink'
+  fields: {
+    name: EntryFieldTypes.Text
+    image: EntryFieldTypes.AssetLink
+    link: EntryFieldTypes.Text
+    order: EntryFieldTypes.Number
+  }
+}
+
+const Contact = async () => {
+  const contactLinkEntries = (
+    await client.getEntries<ContactLinkEntries>({
+      content_type: 'contactLink',
+    })
+  ).items
+
   return (
     <section className="contact__container">
       <h2 className="contact__header">Kontakt</h2>
 
       <div className="contact__social-media-wrapper">
-        <a
-          className="social-media__link"
-          href="mailto:forskoleuppror@gmail.com"
-        >
-          <Image
-            width={30}
-            height={30}
-            className="social-media__img"
-            src={img.at}
-            alt="Email link"
-          />
-          forskoleuppror@gmail
-        </a>
-        <a
-          className="social-media__link"
-          href="https://www.facebook.com/groups/forskoleupproret"
-        >
-          <Image
-            width={30}
-            height={30}
-            className="social-media__img"
-            src={img.facebook}
-            alt="facebook link"
-          />
-          Facebook
-        </a>
-        <a
-          className="social-media__link"
-          href="https://www.instagram.com/forskoleupproret/?hl=sv"
-        >
-          <Image
-            width={30}
-            height={30}
-            className="social-media__img"
-            src={img.insta}
-            alt="instagram link"
-          />
-          Instagram
-        </a>
-        <a className="social-media__link" href="https://x.com/forskoleuppror">
-          <Image
-            width={30}
-            height={30}
-            className="social-media__img"
-            src={img.xLogo}
-            alt="X link"
-          />
-          X.com
-        </a>
-        <a
-          className="social-media__link"
-          href="https://www.linkedin.com/in/förskoleupproret-nationell-2839a9168"
-        >
-          <Image
-            width={30}
-            height={30}
-            className="social-media__img"
-            src={img.linkedin}
-            alt="linkedin link"
-          />
-          LinkedIn
-        </a>
-        <a
-          className="social-media__link"
-          href="https://youtube.com/channel/UCbXox7QE2ShqjRQYp4Pqw3w"
-        >
-          <Image
-            width={30}
-            height={30}
-            className="social-media__img"
-            src={img.youtube}
-            alt="youtube link"
-          />
-          YouTube
-        </a>
+        {contactLinkEntries
+          .sort((a, b) => a.fields.order - b.fields.order)
+          .map(({ fields: { name, link, image } }) => (
+            <a key={name} className="social-media__link" href={link}>
+              <Image
+                width={30}
+                height={30}
+                className="social-media__img"
+                src={`https:${getImage(image).fields?.file.url}`}
+                alt={getImage(image).fields?.title ?? ''}
+              />
+              {name}
+            </a>
+          ))}
       </div>
     </section>
   )
